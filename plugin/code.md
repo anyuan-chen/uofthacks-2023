34ae0fd7518ce956a249e24e86c0b588854f23cb
Andrew Chen <andrew.chen.anyuan@gmail.com>
2024-01-26 23:53:43 -0500

    util, helpers

--- plugin/code.md
+++ plugin/code.md
@@ -1,3 +1,18 @@
 # Application Architecture
 
 # We want to traverse down the Figma DOM with DFS, parsing every level of node into a useful JSX component along the way
+
+Algorithm:
+1. If current node cannot be broken down further (ie. button, form)
+    1. Parse node into a useful format
+    1. Use ML to classify the node in said useful format
+    2. Parse button type and styling attributes into a useful JSX document
+    3. Return said jsx document
+2. If the current node can be broken down further
+    1. Determine the direction of the elements (vertical or horizontal?)
+    2. Determine the spacing between the chidren 
+    3. Recursively run this algorithm on each of the children elements
+    4. Nest the children with an appropriate div
+
+Key observations:
+1. buttons have centered text, inputs typically have left aligned text
---
+++ plugin/src/code.ts
@@ -0,0 +1,119 @@
+// This plugin will generate a sample codegen plugin
+// that appears in the Element tab of the Inspect panel.
+
+import getFormMap from "./formRecursionMapFormation";
+import { RGBAToHSLA } from "./util";
+
+// This file holds the main code for plugins. Code in this file has access to
+// the *figma document* via the figma global object.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+
+export let imports = "";
+
+function getOverlayFrame(destinationId: string) {}
+
+//heuristic - if a text is right above a frame that gets detected as a node, then make it a form label
+
+/*
+  distanceBetweenParentChild(frame, child) calculates how far the borders are from the parent frame borders
+
+  assumes that the parent fully contains the child, 
+  ie. all four corners of the child are within the parent rectangle
+*/
+// const distanceBetweenParentChild = (
+//   frame: FrameNode,
+//   child: TextNode | FrameNode
+// ) => {
+//   const frameBox = frame.absoluteRenderBounds;
+//   const childBox = child.absoluteRenderBounds;
+
+//   if (!frameBox) {
+//     const msg = `frame box with id ${frame.id} is null`;
+//     console.error(msg);
+//     throw msg;
+//   } else if (!childBox) {
+//     const msg = `child box with id ${frame.id} is null`;
+//     console.error(msg);
+//     throw msg;
+//   }
+
+//   const leftPadding = childBox.x - frameBox?.x;
+//   const rightPadding =
+//     frameBox.x + frameBox.width - (childBox.x + childBox.width);
+
+//   const topPadding = childBox.y - frameBox.y;
+//   const bottomPadding =
+//     frameBox.y + frameBox.height - (childBox.y + childBox.height);
+
+//   return {
+//     leftPadding,
+//     rightPadding,
+//     topPadding,
+//     bottomPadding,
+//   };
+// };
+
+// const getFills = (node: FrameNode | TextNode) => {
+//   const fills = [];
+//   if (node.fills != figma.mixed && node.fills[0].type == "SOLID") {
+//     for (const fill of node.fills) {
+//       if (fill.type === "SOLID") {
+//         const hsl = RGBAToHSLA(
+//           fill.color.r,
+//           fill.color.g,
+//           fill.color.b,
+//           fill.opacity || 1
+//         );
+//         fills.push(hsl);
+//         // const opacity = fill.opacity;
+//       }
+//     }
+//   }
+//   return fills;
+// };
+
+// function serializeNode(node: FrameNode | TextNode) {
+//   if (node.type === "TEXT") {
+//     const textNode = node;
+//     const fontSize = textNode.fontSize;
+//     const fontWeight = textNode.fontWeight;
+//   } else {
+//     const paddingLevels = [];
+//     for (const child of node.children) {
+//       child.absoluteBoundingBox;
+//     }
+//   }
+// }
+
+// This provides the callback to generate the code.
+figma.codegen.on("generate", async (event: CodegenEvent) => {
+  imports = "";
+  const root = figma.currentPage.selection[0];
+  if (root.type != "FRAME") {
+    console.error("select a frame");
+    const result: CodegenResult[] = [
+      {
+        language: "PLAINTEXT",
+        code: "",
+        title: "plugin",
+      },
+    ];
+    return result;
+  }
+  figma.skipInvisibleInstanceChildren = true;
+  const formMap = getFormMap(root);
+  //TODO write traverse code
+//   const program = await traverse();
+  const program = ""
+  console.log(await program);
+
+  const result: CodegenResult[] = [
+    {
+      language: "PLAINTEXT",
+      code: await program,
+      title: "Codegen Plugin",
+    },
+  ];
+  return result;
+});
