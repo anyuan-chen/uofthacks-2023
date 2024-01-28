03c5ddf6ba60933743d721487b539b850a51987d
magicalsoup <amagicalsoup@gmail.com>
2024-01-28 00:32:06 -0500

    add

---
+++ frontend/src/app/capsule/journeyData/reddit.tsx
@@ -0,0 +1,153 @@
+const reddit = () => {
+  return (
+    <div className="flex-col">
+      {" "}
+      <div className="flex flex-col gap-y-9">
+        <div className="flex-row">
+          {" "}
+          <div className="flex flex-row gap-x-5">
+            <h1 className="text-4xl text-black font-bold">reddit</h1>
+            <h2 className="text-lg text-black font-bold">r/leagueoflegends</h2>
+          </div>{" "}
+        </div>
+        <div className="flex flex-col gap-y-7">
+          <div className="flex-row">
+            {" "}
+            <div className="flex-row">
+              {" "}
+              <div className="flex flex-row gap-x-9">
+                <div className="flex-col">
+                  {" "}
+                  <div className="flex flex-col gap-y-1.5">
+                    <img
+                      style={{
+                        width: "24px",
+                        height: "22px",
+                        objectFit: "cover",
+                      }}
+                      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAWCAYAAAArdgcFAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGsSURBVHgB7ZQ7SANBEIb/jWckIHaChZXPIqVa+WgVK9FGQUSsRNQuPhDxgSAJqIUWQVB8gGChiKQTbGzstLHQIkgCIsSoYDQmd7vj3QWM8S7mkmjnD8Oyt3Pfzu7ODCNV+CPZrDrS52g9FstwEfRD7m4BhR9hWWRBSuSZoh31RNWg2EAb8dibld8oI1wITvHxXqIa6HCqBSnzQ/p3kS+cuycT0K9WpZ5geUqFi9zh7+seHWSAqyYqQdGDzR83YPoOhncA4qdHKBjshMTSZ4ewO8C2T8DqGk3XzeGBG6CnCSwUQsaEKC0D7Z/BVl5lWDOkIr8LAn2tlsB6dKF7iP52KOEHQwWkRK5EnoCuZkj+K3UFWSlW4YTddwEmFcIQOXEFfHYY0m32YE1FakC0OAouRCpcK2nZM4ai4z0gj05j2/WCr84k5xpLXpqDfWsFvyH72gLkjQSLvRzukMPVhwLtDNpOOVyJ4bTFxeBeH9jrxCBJcjTpxBKD7fIchYHrjFy50glyNiQmLMkQjpL0jYtPj5hW5ndT3C7VW5ialDakrK7H3NlyP89F/3BTfQAZiwek7MasOAAAAABJRU5ErkJggg=="
+                    />
+                    <h3 className="text-xs text-black font-bold">2000</h3>
+                  </div>{" "}
+                </div>
+                <div className="flex-col">
+                  {" "}
+                  <div className="flex flex-col gap-y-1.5">
+                    <div className="flex flex-col gap-y-0">
+                      <h3 className="text-xs text-black font-bold">
+                        TSM VS SKT WORLD FINALS POST MATCH THREAD
+                      </h3>
+                      <div className="flex-row">
+                        {" "}
+                        <div className="flex flex-row gap-x-4">
+                          <p className="text-xs text-gray-700 font-normal">
+                            submitted 1 hour ago
+                          </p>
+                          <p className="text-xs text-gray-700 font-normal">
+                            by NAFAN
+                          </p>
+                        </div>{" "}
+                      </div>
+                    </div>
+                    <p className="text-xs text-gray-700 font-normal">
+                      10115 comments
+                    </p>
+                  </div>{" "}
+                </div>
+              </div>{" "}
+            </div>{" "}
+          </div>
+          <div className="flex-row">
+            {" "}
+            <div className="flex-row">
+              {" "}
+              <div className="flex flex-row gap-x-9">
+                <div className="flex-col">
+                  {" "}
+                  <div className="flex flex-col gap-y-1.5">
+                    <img
+                      style={{
+                        width: "24px",
+                        height: "22px",
+                        objectFit: "cover",
+                      }}
+                      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAWCAYAAAArdgcFAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGsSURBVHgB7ZQ7SANBEIb/jWckIHaChZXPIqVa+WgVK9FGQUSsRNQuPhDxgSAJqIUWQVB8gGChiKQTbGzstLHQIkgCIsSoYDQmd7vj3QWM8S7mkmjnD8Oyt3Pfzu7ODCNV+CPZrDrS52g9FstwEfRD7m4BhR9hWWRBSuSZoh31RNWg2EAb8dibld8oI1wITvHxXqIa6HCqBSnzQ/p3kS+cuycT0K9WpZ5geUqFi9zh7+seHWSAqyYqQdGDzR83YPoOhncA4qdHKBjshMTSZ4ewO8C2T8DqGk3XzeGBG6CnCSwUQsaEKC0D7Z/BVl5lWDOkIr8LAn2tlsB6dKF7iP52KOEHQwWkRK5EnoCuZkj+K3UFWSlW4YTddwEmFcIQOXEFfHYY0m32YE1FakC0OAouRCpcK2nZM4ai4z0gj05j2/WCr84k5xpLXpqDfWsFvyH72gLkjQSLvRzukMPVhwLtDNpOOVyJ4bTFxeBeH9jrxCBJcjTpxBKD7fIchYHrjFy50glyNiQmLMkQjpL0jYtPj5hW5ndT3C7VW5ialDakrK7H3NlyP89F/3BTfQAZiwek7MasOAAAAABJRU5ErkJggg=="
+                    />
+                    <h3 className="text-xs text-black font-bold">2000</h3>
+                  </div>{" "}
+                </div>
+                <div className="flex-col">
+                  {" "}
+                  <div className="flex flex-col gap-y-1.5">
+                    <div className="flex flex-col gap-y-0">
+                      <h3 className="text-xs text-black font-bold">
+                        NA IS SUCH A BAD REGION
+                      </h3>
+                      <div className="flex-row">
+                        {" "}
+                        <div className="flex flex-row gap-x-4">
+                          <p className="text-xs text-gray-700 font-normal">
+                            submitted 1 hour ago
+                          </p>
+                          <p className="text-xs text-gray-700 font-normal">
+                            by NAFAN
+                          </p>
+                        </div>{" "}
+                      </div>
+                    </div>
+                    <p className="text-xs text-gray-700 font-normal">
+                      10115 comments
+                    </p>
+                  </div>{" "}
+                </div>
+              </div>{" "}
+            </div>{" "}
+          </div>
+          <div className="flex-row">
+            {" "}
+            <div className="flex flex-row gap-x-9">
+              <div className="flex-col">
+                {" "}
+                <div className="flex flex-col gap-y-2.5">
+                  <img
+                    style={{
+                      width: "24px",
+                      height: "22px",
+                      objectFit: "cover",
+                    }}
+                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAWCAYAAAArdgcFAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGsSURBVHgB7ZQ7SANBEIb/jWckIHaChZXPIqVa+WgVK9FGQUSsRNQuPhDxgSAJqIUWQVB8gGChiKQTbGzstLHQIkgCIsSoYDQmd7vj3QWM8S7mkmjnD8Oyt3Pfzu7ODCNV+CPZrDrS52g9FstwEfRD7m4BhR9hWWRBSuSZoh31RNWg2EAb8dibld8oI1wITvHxXqIa6HCqBSnzQ/p3kS+cuycT0K9WpZ5geUqFi9zh7+seHWSAqyYqQdGDzR83YPoOhncA4qdHKBjshMTSZ4ewO8C2T8DqGk3XzeGBG6CnCSwUQsaEKC0D7Z/BVl5lWDOkIr8LAn2tlsB6dKF7iP52KOEHQwWkRK5EnoCuZkj+K3UFWSlW4YTddwEmFcIQOXEFfHYY0m32YE1FakC0OAouRCpcK2nZM4ai4z0gj05j2/WCr84k5xpLXpqDfWsFvyH72gLkjQSLvRzukMPVhwLtDNpOOVyJ4bTFxeBeH9jrxCBJcjTpxBKD7fIchYHrjFy50glyNiQmLMkQjpL0jYtPj5hW5ndT3C7VW5ialDakrK7H3NlyP89F/3BTfQAZiwek7MasOAAAAABJRU5ErkJggg=="
+                  />
+                  <h3 className="text-xs text-black font-bold">-200</h3>
+                </div>{" "}
+              </div>
+              <div className="flex-col">
+                {" "}
+                <div className="flex flex-col gap-y-1.5">
+                  <div className="flex flex-col gap-y-0.5">
+                    <h3 className="text-xs text-black font-bold">
+                      Why can’t i climb out of iron 4
+                    </h3>
+                    <div className="flex-row">
+                      {" "}
+                      <div className="flex flex-row gap-x-2.5">
+                        <p className="text-xs text-gray-700 font-normal">
+                          submitted 8 hours ago
+                        </p>
+                        <p className="text-xs text-gray-700 font-normal">
+                          by iamchallenjour
+                        </p>
+                      </div>{" "}
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-700 font-normal">
+                    1015 comments
+                  </p>
+                </div>{" "}
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+      </div>{" "}
+    </div>
+  );
+};
--- frontend/src/app/journey/page.tsx
+++ frontend/src/app/journey/page.tsx
@@ -8,7 +8,64 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 const Page = () => {
   return (
     <>
-      
+      <div className="flex-col">
+        {" "}
+        <div className="flex flex-col gap-y-44">
+          <div className="flex flex-col gap-y-12">
+            <h1 className="text-5xl text-black font-bold">About Me</h1>
+            <div className="flex flex-col gap-y-3.5">
+              <div className="flex-row">
+                {" "}
+                <div className="flex flex-row gap-x-14">
+                  <div className="flex-col">
+                    {" "}
+                    <div className="flex flex-col gap-y-2.5">
+                      <p className="text-2xl text-gray-700 font-medium">
+                        First name
+                      </p>
+                      <p className="text-lg text-gray-700 font-normal">
+                        Andrew
+                      </p>
+                    </div>{" "}
+                  </div>
+                  <div className="flex-col">
+                    {" "}
+                    <div className="flex flex-col gap-y-3.5">
+                      <p className="text-2xl text-gray-700 font-medium">
+                        Last name:
+                      </p>
+                      <p className="text-lg text-gray-700 font-normal">Chen</p>
+                    </div>{" "}
+                  </div>
+                </div>{" "}
+              </div>
+              <div className="flex-col">
+                {" "}
+                <div className="flex flex-col gap-y-1">
+                  <p className="text-2xl text-gray-700 font-medium">
+                    My favorite Hobby
+                  </p>
+                  <p className="text-lg text-gray-700 font-normal">
+                    Love to play tft during class! (Oop sorry, I meant do actual
+                    chemistry :p)
+                  </p>
+                </div>{" "}
+              </div>
+            </div>
+          </div>
+          <div className="flex-row">
+            {" "}
+            <div className="flex flex-row gap-x-36">
+              <Button variant="destructive" className="pt-3.5 pb-3">
+                Learn more
+              </Button>
+              <Button variant="destructive" className="pt-3.5 pb-3">
+                Close
+              </Button>
+            </div>{" "}
+          </div>
+        </div>{" "}
+      </div>
     </>
   );
 };
